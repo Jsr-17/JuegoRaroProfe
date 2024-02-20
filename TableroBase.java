c4a010039d615274b991adb514463701a3c9c367
Jsr-17 <jarmoreno5@gmail.com>
2024-02-20 14:06:08 +0100

    Tablero Base Casi terminado

--- TableroBase.java
+++ TableroBase.java
@@ -1,4 +1,50 @@
-public class TableroBase {
+public  abstract class TableroBase {
 
+    int[][] tablero;
+    final int FIL=4;   
+    final int COL=4;
     
+    public void muestraTablero(){
+        for(int i=0; i<4;i++){
+            for(int j=0;j<4;j++){
+                System.out.print(this.tablero[i][j]);
+            }
+            System.out.println();
+        }
+    }
+    public int generaNumeroAleatorio(){
+
+        return (int)(Math.random()*4);
+    }
+
+    public void generaPosicionesAleatorias(){
+        int numero1;
+        int numero2;
+
+        for(int i =0;i<2;i++){
+            numero1=generaNumeroAleatorio();
+            numero2=generaNumeroAleatorio();
+            
+            if(tablero[numero1][numero2]==2){
+                tablero[numero1][numero2]=2;
+                i--;
+            }
+            tablero[numero1][numero2]=2;
+        }
+        
+    }
+
+    public TableroBase(){
+        tablero =new int [FIL][COL];
+        generaPosicionesAleatorias();
+    }
+    
+    public static void main(String[] args) {
+        TableroBase uno = new TableroBase() {
+            
+        };
+        uno.muestraTablero();
+
+    }
+
 }
